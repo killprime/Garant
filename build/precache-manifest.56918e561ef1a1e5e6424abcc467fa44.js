be0bd86a4c5e064ecff7141c756fb19151f20d8d
killprime <killprime@mail.ru>
2020-06-07 20:50:53 +1000

    Сборка

---
+++ build/precache-manifest.56918e561ef1a1e5e6424abcc467fa44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ce9423c81db629ad1f34031b857625",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59011d7d800763f10ac7",
+    "url": "/static/css/main.5cc4a6f9.chunk.css"
+  },
+  {
+    "revision": "6f48c9a0b00c95f59cc8",
+    "url": "/static/js/2.52782fb8.chunk.js"
+  },
+  {
+    "revision": "d0b30a1909a753c393e134caf98fa9bb",
+    "url": "/static/js/2.52782fb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59011d7d800763f10ac7",
+    "url": "/static/js/main.30e222c6.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
